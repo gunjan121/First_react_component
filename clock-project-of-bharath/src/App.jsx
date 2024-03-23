84b754d890853bf0bfe40d139032af0e5a40c38f
gunjan121 <gunjanplrr@gmail.com>
2024-03-23 19:37:30 +0530

    clock project

---
+++ clock-project-of-bharath/src/App.jsx
@@ -0,0 +1,19 @@
+import ClockHeading from "./component/clockHeading.jsx"
+import ClockSlogan from "./component/clockSlogan.jsx"
+import CurrentTime from "./component/currentTime.jsx"
+import './App.css'
+
+function App() 
+{
+  return (
+    <center>
+      <ClockHeading/>
+      <ClockSlogan/>
+      <CurrentTime/>
+    </center>
+    );
+}
+  
+  
+
+export default App
